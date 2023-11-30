3e8f7f72033fbd51de2b6076d0f48a0b3b06fd12
ManVeraM <84684731+ManVeraM@users.noreply.github.com>
2023-11-30 19:17:45 -0300

    Added: Hash password rut and mail validations

---
+++ Backend/Controllers/AuthController.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Backend;
+using Backend.Models;
+using Microsoft.AspNetCore.DataProtection;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using System.Security.Claims;
+using BCrypt.Net;
+
+
+namespace Dumbo.Controllers{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController: ControllerBase{
+        private readonly string secretKey;
+        private readonly DataContext _context;
+
+        public AuthenticationController(IConfiguration config, DataContext context){
+            secretKey = config.GetSection("Settings").GetSection("SecretKey").ToString();
+            _context = context;
+
+        }
+
+
+   
+
+
+        /// <summary>
+        /// Validate if the credentials matches with the registered admin
+        /// </summary>
+        /// <param name="admin">Model that contains a admin that wants to log into the system</param>
+        /// <returns>
+        /// If crendentials are correct return status 200 with the token
+        /// Else return status 401 with a message the invalid credentials
+        /// </returns>
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] User user)
+        {
+            
+            var existingUser = await _context.Users.FirstOrDefaultAsync(a => a.Rut == user.Rut);
+            if (existingUser == null)
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, new { message = "Invalid credentials" });
+            }
+
+            var passwordMatches = BCrypt.Net.BCrypt.Verify(user.Password, existingUser.Password);
+            if (!passwordMatches)
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, new { message = "Invalid credentials" });
+            }
+            
+
+            var keyBytes = Encoding.ASCII.GetBytes(secretKey);
+            var claims = new ClaimsIdentity();
+            claims.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Rut));
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = claims,
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(keyBytes), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenConfig = tokenHandler.CreateToken(tokenDescriptor);
+
+            string CreatedToken = tokenHandler.WriteToken(tokenConfig);
+
+            return StatusCode(StatusCodes.Status200OK, new { token = CreatedToken });
+        }
+    }
+
+}
--- Backend/Controllers/UsersController.cs
+++ Backend/Controllers/UsersController.cs
@@ -103,5 +103,90 @@ namespace Backend.Controllers
         {
             return _context.Users.Any(e => e.Id == id);
         }
+
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<User>> UserRegister(User user)
+        {
+            // Validaciones personalizadas
+            if (!_context.Users.All(u => u.Email != user.Email))
+            {
+                ModelState.AddModelError("Email", "El correo ya está en uso.");
+            }
+
+            if (!_context.Users.All(u => u.Rut != user.Rut))
+            {
+                ModelState.AddModelError("Rut", "El RUT ya está en uso.");
+            }
+
+            if (!IsValidRut(user.Rut))
+            {
+                ModelState.AddModelError("Rut", "El RUT no es válido según el algoritmo del módulo 11.");
+            }
+
+            if (!IsValidEmail(user.Email))
+            {
+                ModelState.AddModelError("Email", "El correo no es válido o no pertenece al dominio permitido.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+
+        private bool IsValidRut(string rut)
+        {
+            // Eliminar puntos y guiones del RUT
+            rut = rut.Replace(".", "").Replace("-", "");
+            Console.WriteLine(rut);
+    
+            // Verificar que el RUT tenga el formato correcto
+            if (!System.Text.RegularExpressions.Regex.IsMatch(rut, @"^[0-9]{8,9}$"))
+            {
+                return false;
+            }
+
+            // Separar el cuerpo y el dígito verificador
+            string cuerpo = rut.Substring(0, rut.Length - 1);
+            char dv = char.ToUpper(rut[rut.Length - 1]);
+
+            // Calcular el dígito verificador esperado
+            int suma = 0;
+            int multiplicador = 2;
+
+            for (int i = cuerpo.Length - 1; i >= 0; i--)
+            {
+                suma += int.Parse(cuerpo[i].ToString()) * multiplicador;
+                multiplicador = multiplicador < 7 ? multiplicador + 1 : 2;
+            }
+
+            int resto = suma % 11;
+            int resultadoEsperado = 11 - resto;
+
+            // Convertir 10 a 'K'
+            char dvEsperado = resultadoEsperado == 10 ? 'K' : resultadoEsperado.ToString()[0];
+
+            // Comparar el dígito verificador calculado con el proporcionado
+            return dv == dvEsperado;
+        }
+
+        // Método para validar el correo según la expresión regular y el dominio permitido
+        private bool IsValidEmail(string email)
+        {
+            // Expresión regular para validar el formato del correo electrónico
+            string emailRegex = @"^[a-zA-Z0-9._-]+@(ucn\.cl|alumnos\.ucn\.cl|disc\.ucn\.cl|ce\.ucn\.cl)$";
+
+            // Verificar el formato del correo y el dominio permitido
+            return System.Text.RegularExpressions.Regex.IsMatch(email, emailRegex);
+        }
+
+        
     }
 }
